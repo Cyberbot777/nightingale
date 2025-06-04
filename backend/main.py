8e52fe367f33cbcc8e09363e1c6aefe8620e4a02
Cyberbot777 <Cyberbot777@users.noreply.github.com>
2025-06-04 13:02:29 -0700

    add new url to CORS

--- backend/main.py
+++ backend/main.py
@@ -21,6 +21,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "https://nightingale-sigma.vercel.app"
+        "https://nightingaleapp.ai"
     ],
     allow_credentials=True,
     allow_methods=["*"],
